feb5144dd0178cc25d0a160f7b45e2a262f668a2
Inialpha <inimfonebong001@gmail.com>
2026-08-20 12:30:58 +0100

    Configure Compose compiler 1.5.14 for Kotlin 1.9.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
 }
 
@@ -39,6 +38,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
